76e884733eb570aa37337cd8a339cf596df7b55b
Shalom Orangi <shalom.orangi@student.moringaschool.com>
2024-01-24 23:27:27 +0300

    pdate

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,18 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        URL="https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+        response= requests.get(URL)
+        return response.content
 
     def load_json(self):
-        pass
+        python_list=[]
+        requests=json.loads(self.get_response_body())
+        for request in requests:
+            python_list.append(request["agency"])
+
+get_requester=GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.load_json()
+
+
